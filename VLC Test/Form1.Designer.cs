be43e7b7d87fbc154d1769d12c235daa1a25d4c4
Steve Hockemeier <shockeme@yahoo.com>
2018-06-04 22:33:23 -0700

    Updated with DVD

--- VLC Test/Form1.Designer.cs
+++ VLC Test/Form1.Designer.cs
@@ -36,6 +36,12 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,17 +58,17 @@
             // 
             this.button1.Location = new System.Drawing.Point(580, 18);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 32);
+            this.button1.Size = new System.Drawing.Size(84, 32);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Load";
+            this.button1.Text = "Play DVD";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(580, 79);
+            this.button2.Location = new System.Drawing.Point(672, 135);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(91, 32);
+            this.button2.Size = new System.Drawing.Size(82, 32);
             this.button2.TabIndex = 2;
             this.button2.Text = "Play";
             this.button2.UseVisualStyleBackColor = true;
@@ -70,9 +76,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(580, 136);
+            this.button3.Location = new System.Drawing.Point(672, 173);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(91, 32);
+            this.button3.Size = new System.Drawing.Size(82, 32);
             this.button3.TabIndex = 3;
             this.button3.Text = "Stop";
             this.button3.UseVisualStyleBackColor = true;
@@ -80,19 +86,78 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(580, 197);
+            this.button4.Location = new System.Drawing.Point(672, 211);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(91, 32);
+            this.button4.Size = new System.Drawing.Size(82, 32);
             this.button4.TabIndex = 4;
             this.button4.Text = "Pause";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(582, 364);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 33);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Title";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(582, 135);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(84, 31);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Load File";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(582, 249);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(98, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(582, 403);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(135, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(688, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Current Time Stamp";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(580, 56);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(84, 28);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Load Filter";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -102,6 +167,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -113,6 +179,12 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- VLC Test/Form1.cs
+++ VLC Test/Form1.cs
@@ -8,45 +8,60 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Timers;
+using System.IO;
 
 namespace VLC_Test
 {
     public partial class Form1 : Form
     {
         private static System.Timers.Timer aTimer;
+        private bool mute;
+        List<string> ActionList = new List<string>();
+        List<string> StartList = new List<string>();
+        List<string> EndList = new List<string>();
+        int listIndex = 0;
 
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string temp;
-
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            if(openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                temp = "file:///" + openFileDialog1.FileName;
-                axVLCPlugin21.playlist.add(temp, openFileDialog1.SafeFileName, null);
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            // Create a timer and set a two second interval.
             aTimer = new System.Timers.Timer();
-            aTimer.Interval = 2000;
+            aTimer.Interval = 20; //20 msecs
 
             // Hook up the Elapsed event for the timer. 
             aTimer.Elapsed += OnTimedEvent;
+            aTimer.SynchronizingObject = this;
 
             // Have the timer fire repeated events (true is the default)
             aTimer.AutoReset = true;
+            mute = false;
 
+        }
+
+        // Load and Play DVD 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string temp;
+            temp = "dvd:///e:/";
+            axVLCPlugin21.playlist.add(temp, null);
+            aTimer.Enabled = true;
+            axVLCPlugin21.playlist.play();
+        }
+
+        // Load File
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                axVLCPlugin21.playlist.add("file:///" + openFileDialog1.FileName, openFileDialog1.SafeFileName, null);
+            }
+        }
+
+        // Play File
+        private void button2_Click(object sender, EventArgs e)
+        {
             // Start the timer
             aTimer.Enabled = true;
-
             axVLCPlugin21.playlist.play();
         }
 
@@ -62,12 +77,47 @@ namespace VLC_Test
 
         private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
-            axVLCPlugin21.audio.toggleMute();
+            textBox2.Text = axVLCPlugin21.input.time.ToString(); // in miliseconds
+
+            /*if (axVLCPlugin21.input.title.track > 0)
+            {
+                if (ActionList[listIndex] == "mute")
+                {
+                    if (axVLCPlugin21.input.time > Int32.Parse(StartList[listIndex]) && axVLCPlugin21.input.time < Int32.Parse(EndList[listIndex]))
+                        axVLCPlugin21.audio.mute = true;
+                    else
+                        axVLCPlugin21.audio.mute = false;
+                }
+                else
+                {
+                    if (axVLCPlugin21.input.time > Int32.Parse(StartList[listIndex]) && axVLCPlugin21.input.time < Int32.Parse(EndList[listIndex]))
+                        axVLCPlugin21.input.time = Int32.Parse(EndList[listIndex]);
+                    listIndex++;
+                }
+            }*/
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)
         {
+            textBox4.Text = axVLCPlugin21.input.time.ToString();
+        }
 
+        private void button7_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader myFilterFile = new StreamReader(openFileDialog1.FileName);
+                do
+                {
+                    string[] FilterFileEntry = myFilterFile.ReadLine().Split(',');
+                    ActionList.Add(FilterFileEntry[0]);
+                    StartList.Add(FilterFileEntry[1]);
+                    EndList.Add(FilterFileEntry[2]);
+
+                } while (!myFilterFile.EndOfStream);
+                myFilterFile.Close();
+            }
         }
     }
 }
